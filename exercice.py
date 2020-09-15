b834cd8e63f324392ee286782f22dee610858f11
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-15 22:53:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -16,7 +16,7 @@ def remove_third_char(string: str) -> str:
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     s = string
     for i in range(0,len(string)):
-        if(string[i] == old_char):
+        if(s[i] == old_char):
 
 
 
