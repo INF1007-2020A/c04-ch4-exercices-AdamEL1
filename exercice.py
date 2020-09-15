5bb97b3564815c5ed4d5ade894cfca40f4d2ea35
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-15 22:56:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -32,7 +32,8 @@ def get_number_of_char(string: str, char: str) -> int:
 def get_number_of_words(sentence: str, word: str) -> int:
     space_count = 0
     for c in sentence:
-        
+        if c == " ":
+            space_count
 
 
 def main() -> None:
