66ad72aa8420ab04e9c2df4698273f370de52daf
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-15 22:55:22 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,8 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    for c in str
+    for c in string:
+        if c == 
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
