6a0df1b122d5b5499e83ba0705c67147a2ea3174
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-21 22:25:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,9 +31,9 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     count = 0
-    for i in range(len(sentence)-2):
+    for i in range(0,len(sentence)-2):
         if word == sentence[i]:
-
+            
 
 
 def main() -> None:
