cc5c86dad2528f109c45fceed21c839137f26a4c
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-21 22:23:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,7 +31,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     count = 0
-    for i in range():
+    for i in range(len()):
         if word == sentence[i]:
 
 
